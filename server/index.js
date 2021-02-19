8eaffa5a96907a6e18f6931f642111bb15625917
Vincent <vpang@law.fordham.edu>
2021-02-18 20:29:06 -0500

    Create Repo

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('*', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+});
+
+app.use((err, req, res, next) => {
+  if(process.env.NODE_ENV !== 'test') console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error');
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {console.log(`Listening on port ${PORT}`)});
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+
+module.exports = {
+  entry: './index.js',
+  mode: 'development',
+  output: {
+    path: __dirname,
+    filename: 'public/bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  }
+}
