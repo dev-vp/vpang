132eb8942413bf93c09867f4343184c0e23a45c2
Vincent <vpang@law.fordham.edu>
2021-02-18 21:06:14 -0500

    Nav Wired Up and Minimal Styling Added

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 class Nav extends React.Component{
 
   render(){
     return (
-      <h1>Hello World</h1>
+      <Link to="/" id="logo" >Vincent Pang</Link>
     )
   }
 }
--- app/main.js
+++ app/main.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
 import {Nav} from './components/index'
+import '../public/stylesheet.css'
 
 class Main extends React.Component{
   render(){
     return (
-      <Nav />
+      <Router>
+        <Nav />
+      </Router>
     )
   }
 }
