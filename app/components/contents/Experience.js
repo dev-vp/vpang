5c579e0c543c042ecc7509f60d87b78455b9cda3
Vincent <vpang@law.fordham.edu>
2021-02-21 20:18:32 -0500

    Experience Component Updated

--- app/components/contents/Experience.js
+++ app/components/contents/Experience.js
@@ -16,6 +16,39 @@ class Experience extends React.Component{
             <p id="deutsche-details">TBD</p>
           </div>
         </div>
+        <div id="boc" className="company">
+          <div className="employed-year">
+            <h3 className="tiffany">2014 - 2016</h3>
+          </div>
+          <div className="company-details">
+            <h3>Bank of China</h3>
+            <h4>Legal and Compliance</h4>
+            <p className="gray-italics">New York, NY</p>
+            <p id="boc-details">TBD</p>
+          </div>
+        </div>
+        <div id="chase" className="company">
+          <div className="employed-year">
+            <h3 className="tiffany">2012 - 2014</h3>
+          </div>
+          <div className="company-details">
+            <h3>J.P. Morgan Chase</h3>
+            <h4>Teller</h4>
+            <p className="gray-italics">Springfield, NY</p>
+            <p id="chase-details">TBD</p>
+          </div>
+        </div>
+        <div id="td-bank" className="company">
+          <div className="employed-year">
+            <h3 className="tiffany">2010 - 2012</h3>
+          </div>
+          <div className="company-details">
+            <h3>TD Bank, N.A.</h3>
+            <h4>Teller</h4>
+            <p className="gray-italics">Mineola, NY</p>
+            <p id="td-bank-details">TBD</p>
+          </div>
+        </div>
       </div>
     )
   }
