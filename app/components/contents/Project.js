564e58d72c5be36b7428a04c4fb2baeaa0fd6b1a
Vincent <vpang@law.fordham.edu>
2021-02-26 15:24:15 -0500

    Project Component Framework Created

--- app/components/contents/Project.js
+++ app/components/contents/Project.js
@@ -4,7 +4,26 @@ class Project extends React.Component{
   render(){
     return (
       <div id="projects-container">
-
+        <div className="project">
+          <h2>Project Name</h2>
+          <p>Sunt dolore in eu aliquip elit. Officia proident pariatur veniam minim Lorem aliquip voluptate. Voluptate occaecat voluptate sit sit non. Sit laborum velit amet proident laborum nulla.</p>
+        </div>
+        <div className="project">
+          <h2>Project Name</h2>
+          <p>Sunt dolore in eu aliquip elit. Officia proident pariatur veniam minim Lorem aliquip voluptate. Voluptate occaecat voluptate sit sit non. Sit laborum velit amet proident laborum nulla.</p>
+        </div>
+        <div className="project">
+          <h2>Project Name</h2>
+          <p>Sunt dolore in eu aliquip elit. Officia proident pariatur veniam minim Lorem aliquip voluptate. Voluptate occaecat voluptate sit sit non. Sit laborum velit amet proident laborum nulla.</p>
+        </div>
+        <div className="project">
+          <h2>Project Name</h2>
+          <p>Sunt dolore in eu aliquip elit. Officia proident pariatur veniam minim Lorem aliquip voluptate. Voluptate occaecat voluptate sit sit non. Sit laborum velit amet proident laborum nulla.</p>
+        </div>
+        <div className="project">
+          <h2>Project Name</h2>
+          <p>Sunt dolore in eu aliquip elit. Officia proident pariatur veniam minim Lorem aliquip voluptate. Voluptate occaecat voluptate sit sit non. Sit laborum velit amet proident laborum nulla.</p>
+        </div>
       </div>
     )
   }
